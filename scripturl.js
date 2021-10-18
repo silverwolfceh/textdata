182626f4231a4c1081f9c47c4cac6728e11b37c8
Vưu Văn Tòng (Eric) <silverwolfceh@users.noreply.github.com>
2021-10-18 19:08:20 +0700

    Update

--- scripturl.js
+++ scripturl.js
@@ -1,1 +1,1 @@
-var app_script_url = "https://script.google.com/macros/s/AKfycbydqSwRLDkjjzPkZemt8-Rx-qmyx39RCP20vgJ__vY0Qs8pKKYot0k0MQwGf-CsNNoF_w/exec";
+var app_script_url = "https://script.google.com/macros/s/AKfycbymPDbjTRskIQzmh2cp_H7B_BgrnIGqyFIQGlzlc7KLSwp7sO27MXlyrW9-jEr3_i2rcw/exec";
